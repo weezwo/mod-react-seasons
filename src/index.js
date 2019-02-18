8f009d248446e9516ab7e2d75777d608562292af
Luisa Scavo <luisascavo@gmail.com>
2019-02-18 08:13:10 -0500

    render SeasonDisplay

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import SeasonDisplay from './SeasonDisplay';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
 
@@ -23,7 +23,7 @@ class App extends React.Component {
     }
 
     if(!this.state.errorMessage && this.state.lat){
-      return <div> Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <div>Loading!</div>
