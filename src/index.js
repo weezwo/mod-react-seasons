09e59b32c2d06783b7712a0c4eed6a7f00906fe8
Luisa Scavo <luisascavo@gmail.com>
2019-02-18 08:51:51 -0500

    add period

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />
     }
 
-    return <Spinner message="Please accept location request" />
+    return <Spinner message="Please accept location request." />
 
   }
 }
